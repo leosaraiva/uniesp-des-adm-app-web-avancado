4c05f18bbc88e714fc92ad9aca79dc2d3d80b5a9
Leo Saraiva <leosaraiv4@gmail.com>
2023-02-28 22:36:07 -0300

    hello-world

---
+++ src/main/java/com/helloworld/api/HellWorldApplication.java
@@ -0,0 +1,13 @@
+package com.helloworld.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellWorldApplication.class, args);
+	}
+
+}
